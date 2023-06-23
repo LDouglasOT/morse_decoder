d31cfe6490ec1196338f023305ff72b161f4ced9
ActiveApps <douglas.luzinda.ot@gmail.com>
2023-06-23 10:57:19 +0300

    morse code decorder

---
+++ decode_morse_code.rb
@@ -0,0 +1,18 @@
+MORSE_CODE = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }.freeze
+  
+  def decode_char(code)
+    MORSE_CODE[code]
+  end
+  
+  def decode_word(word)
+    letters = word.split
+    decoded_word = letters.map { |letter| decode_char(letter) }
+    decoded_word.join
+  end
